c71553190c26d81ac8addafc24ab8baf3057a5d2
FarahZkr <farahzakari@gmail.com>
2023-11-26 01:35:22 -0500

    Fixed Part 1

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pandas as pd
 import gensim.downloader
 import random
 
-# region Part-1
 # ======================================
+# region Part-1
 
 model_name = 'word2vec-google-news-300'
 word2vec_model = gensim.downloader.load(model_name)
 
 file_path = 'A2-DataSet/synonym.csv'
 dataset = pd.read_csv(file_path)
+dataset_length = len(dataset)
 vocab_size = len(word2vec_model.index_to_key)
 
 
@@ -30,6 +31,9 @@ def find_closest_synonym(questionWord, synonym_words):
                 similarities = [(word, word2vec_model.similarity(questionWord, word)) for word in synonym_words]
                 closest_synonym = max(similarities, key=lambda x: x[1])[0]
                 return closest_synonym
+            else:
+                closest_word = word2vec_model.most_similar(questionWord, topn=1)[0][0].lower()
+                return closest_word
         else:
             return "Synonyms not found."
     except KeyError:
@@ -37,7 +41,6 @@ def find_closest_synonym(questionWord, synonym_words):
 
 
 correct_labels = 0
-total_guesses = 0
 for index, row in dataset.iterrows():
     question = row['question']
     answer = row['answer']
@@ -46,25 +49,28 @@ for index, row in dataset.iterrows():
     to_write = f"{question},{answer}"
 
     similar_word = find_closest_synonym(question, synonyms)
-    if similar_word != "Synonyms not found.":
+    if similar_word != "Synonyms not found." and similar_word in synonyms:
         to_write += f",{similar_word}"
-        total_guesses += 1
         if similar_word != answer:
             to_write += ",wrong"
         else:
             to_write += ",correct"
             correct_labels += 1
+    elif similar_word != "Synonyms not found.":
+        to_write += f",{similar_word},guess"
+        dataset_length -= 1
     else:
-        to_write += f"{random.choice(word2vec_model.index_to_key)},guess"
+        to_write += f",{random.choice(word2vec_model.index_to_key).lower()},guess"
+        dataset_length -= 1
     to_write += "\n"
     write_to_file_1(to_write)
     to_write = ""
-if total_guesses != 0:
-    total = correct_labels / total_guesses
-else:
-    total = 1
-analysis_file = f"{model_name},{vocab_size},{correct_labels},{total_guesses},{total}"
+
+
+total = correct_labels / dataset_length
+analysis_file = f"{model_name},{vocab_size},{correct_labels},{dataset_length},{total}"
 write_to_file_2(analysis_file)
+
 # endregion
 # ======================================
 # region Part-2
@@ -73,25 +79,25 @@ write_to_file_2(analysis_file)
 # Twitter model: glove-twitter-100
 # Wiki-GigaWord: glove-wiki-gigaword-100
 
-twitter_model_name = 'glove-twitter-100'
-twitter_model = gensim.downloader.load(twitter_model_name)
-
-giga_model_name = 'glove-wiki-gigaword-100'
-giga_model = gensim.downloader.load(giga_model_name)
-
-# Same models with different sizes:
-# Twitter: glove-twitter-50
-# Wiki-GigaWord: glove-wiki-gigaword-300
-
-twitter_model_name2 = 'glove-twitter-50'
-twitter_model2 = gensim.downloader.load(twitter_model_name2)
-
-giga_model_name2 = 'glove-wiki-gigaword-300'
-giga_model2 = gensim.downloader.load(giga_model_name2)
+# twitter_model_name = 'glove-twitter-100'
+# twitter_model = gensim.downloader.load(twitter_model_name)
+#
+# giga_model_name = 'glove-wiki-gigaword-100'
+# giga_model = gensim.downloader.load(giga_model_name)
+#
+# # Same models with different sizes:
+# # Twitter: glove-twitter-50
+# # Wiki-GigaWord: glove-wiki-gigaword-300
+#
+# twitter_model_name2 = 'glove-twitter-50'
+# twitter_model2 = gensim.downloader.load(twitter_model_name2)
+#
+# giga_model_name2 = 'glove-wiki-gigaword-300'
+# giga_model2 = gensim.downloader.load(giga_model_name2)
 
 
 # endregion
 # ======================================
 # region Part-3
 
-#endregion
+# endregion
